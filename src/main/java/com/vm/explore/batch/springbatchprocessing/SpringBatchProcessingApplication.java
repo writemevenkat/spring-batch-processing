6f03c2d1ec3955ac6e8dd6eb9d7c57e808663d6b
writemevenkat <writemevenkat@gmail.com>
2020-03-25 19:48:50 -0500

    initial commit

---
+++ src/main/java/com/vm/explore/batch/springbatchprocessing/SpringBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.vm.explore.batch.springbatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchProcessingApplication.class, args);
+	}
+
+}
